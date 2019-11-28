e24b52be8bb2005d7779a7068cb540bd2e50606b
AnithaM-CTS <58251743+AnithaM-CTS@users.noreply.github.com>
2019-11-28 01:10:10 -0800

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,3 @@
+insert into item(id,name,description,price) values (1001,'1stItem','1stItem desc',100.00);
+insert into item(id,name,description,price) values (1002,'2ndItem','2ndItem desc',200.00);
+insert into item(id,name,description,price) values (1003,'3rdItem','3rdItem desc',300.00);
